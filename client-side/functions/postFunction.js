2f6c52645871d375ef52f65faeaa11b33facc20a
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-09 23:56:24 -0400

    add

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -14,7 +14,7 @@ exports.handler = async function(event, context) {
   
 
   const auth = new google.auth.GoogleAuth({
-      keyFile: "credentials.json",
+      keyFile: "./client-side/credentials.json",
       scopes: "https://www.googleapis.com/auth/spreadsheets"
   });
 
