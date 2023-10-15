f25c977ce8b434a9c0c72f1e532e05b935ce4269
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-15 01:12:20 -0400

    f

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -71,17 +71,17 @@ export async function handler(event, context) {
       await updateValues(`Enter Data Here!B${i}`, newProductValues[`item${i-6}`])
   }
   
-  for(let i = 18; i <= 28; i++) {
-      await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
-  }
+  // for(let i = 18; i <= 28; i++) {
+  //     await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
+  // }
   
-  for(let i = 18; i <= 28; i++) {
-      await updateValues(`Enter Data Here!D${i}`, newProductValues[`item${i+16}`])
-  }
+  // for(let i = 18; i <= 28; i++) {
+  //     await updateValues(`Enter Data Here!D${i}`, newProductValues[`item${i+16}`])
+  // }
   
-  for(let i = 18; i <= 28; i++) {
-      await updateValues(`Enter Data Here!E${i}`, newProductValues[`item${i+27}`])
-  }
+  // for(let i = 18; i <= 28; i++) {
+  //     await updateValues(`Enter Data Here!E${i}`, newProductValues[`item${i+27}`])
+  // }
 
   await updateValues("Enter Data Here!B2", newCustomerValues.nameOne);
   await updateValues("Enter Data Here!B3", newCustomerValues.nameTwo);
