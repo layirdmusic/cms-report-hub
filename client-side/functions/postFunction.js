509d7f355716a07321f780c982dab65c6054b03e
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-15 01:16:03 -0400

    d

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -71,9 +71,9 @@ export async function handler(event, context) {
       await updateValues(`Enter Data Here!B${i}`, newProductValues[`item${i-6}`])
   }
   
-  // for(let i = 18; i <= 28; i++) {
-  //     await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
-  // }
+  for(let i = 18; i <= 28; i++) {
+      await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
+  }
   
   // for(let i = 18; i <= 28; i++) {
   //     await updateValues(`Enter Data Here!D${i}`, newProductValues[`item${i+16}`])
