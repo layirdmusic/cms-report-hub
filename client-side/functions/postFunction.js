86f03fae82d0072007ede05739a492a315034638
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-15 03:24:26 -0400

    h

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -55,19 +55,13 @@ export async function handler(event, context) {
   for(let i = 18; i <= 28; i++) {
       await updateValues(`Enter Data Here!A${i}`, newProductValues[`item${i-17}`])
 
-      await delay(1000);
-
       await updateValues(`Enter Data Here!B${i}`, newProductValues[`item${i-6}`])
 
-      await delay(1000);
+      await delay(5000);
 
       await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
 
-      await delay(1000);
-
       await updateValues(`Enter Data Here!D${i}`, newProductValues[`item${i+16}`])
-
-      await delay(1000);
 
       await updateValues(`Enter Data Here!E${i}`, newProductValues[`item${i+27}`])
   }
