6115c80fb71a39c7cb7ae40cab6ec58e858cefa2
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-15 03:35:35 -0400

    j

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -1,87 +1,71 @@
-import { google } from "googleapis";
-
 export async function handler(event, context) {
-
-  let newCustomerValues
-  let newProductValues
-  const productValues = []
-  for(let i = 1; i <= 55; i++) {
-    productValues.push(`item${[i]}`)
-  }
-
-  try {
-
-    const customerValues = ["nameOne", "nameTwo", "date", "threePart", "poNum", "receivedBy", "bolNum", "carrier", "lbs", "totalCount", "vendor"]
-
-    newCustomerValues = {}
-    const responseBody = JSON.parse(event.body)
-
-    for(let i = 0; i < customerValues.length; i++){
-      newCustomerValues[customerValues[i]] = responseBody[customerValues[i]]
-    }
-
-    newProductValues = {}
-    for(let i = 0; i < productValues.length; i++){
-      newProductValues[productValues[i]] = responseBody[productValues[i]]
-    }
-
-  } catch(error) {
-    console.error('Error parsing JSON:', error)
-  }
-  
-
   const auth = new google.auth.GoogleAuth({
-      keyFile: "./client-side/credentials.json",
-      scopes: "https://www.googleapis.com/auth/spreadsheets"
+    keyFile: "./client-side/credentials.json",
+    scopes: "https://www.googleapis.com/auth/spreadsheets",
   });
 
   const client = await auth.getClient();
-
   const googleSheets = google.sheets({ version: "v4", auth: client });
   const spreadsheetId = "1SsmyuqEiCMH8mCria-Ea2v53CCJC43yMWYEQGesQ27A";
+  const requests = [];
 
-  const updateValues = async (range, value) => {
-    return googleSheets.spreadsheets.values.update({
-      auth,
-      spreadsheetId,
-      range,
-      valueInputOption: "USER_ENTERED",
-      resource: {
-        values: [[value]],
+  const addUpdateRequest = (sheetId, range, value) => {
+    requests.push({
+      updateCells: {
+        range: {
+          sheetId,
+          startRowIndex: range[0],
+          endRowIndex: range[1],
+          startColumnIndex: range[2],
+          endColumnIndex: range[3],
+        },
+        fields: "userEnteredValue",
+        rows: [
+          {
+            values: [{ userEnteredValue: { stringValue: value } }],
+          },
+        ],
       },
     });
   };
-   
-  for(let i = 18; i <= 28; i++) {
-      await updateValues(`Enter Data Here!A${i}`, newProductValues[`item${i-17}`])
 
-      await updateValues(`Enter Data Here!B${i}`, newProductValues[`item${i-6}`])
-
-      await delay(5000);
-
-      await updateValues(`Enter Data Here!C${i}`, newProductValues[`item${i+5}`])
-
-      await updateValues(`Enter Data Here!D${i}`, newProductValues[`item${i+16}`])
-
-      await updateValues(`Enter Data Here!E${i}`, newProductValues[`item${i+27}`])
+  // Add update requests for product values
+  for (let i = 18; i <= 28; i++) {
+    addUpdateRequest(1, [i, i + 1, 0, 1], newProductValues[`item${i - 17}`]);
+    addUpdateRequest(1, [i, i + 1, 1, 2], newProductValues[`item${i - 6}`]);
+    addUpdateRequest(1, [i, i + 1, 2, 3], newProductValues[`item${i + 5}`]);
+    addUpdateRequest(1, [i, i + 1, 3, 4], newProductValues[`item${i + 16}`]);
+    addUpdateRequest(1, [i, i + 1, 4, 5], newProductValues[`item${i + 27}`]);
   }
 
+  // Add update requests for customer values
+  const customerValues = [
+    "nameOne",
+    "nameTwo",
+    "date",
+    "threePart",
+    "poNum",
+    "receivedBy",
+    "bolNum",
+    "carrier",
+    "lbs",
+    "totalCount",
+    "vendor",
+  ];
+  for (let i = 0; i < customerValues.length; i++) {
+    addUpdateRequest(1, [i + 1, i + 2, 1, 2], newCustomerValues[customerValues[i]]);
+  }
 
-  await updateValues("Enter Data Here!B2", newCustomerValues.nameOne);
-  await updateValues("Enter Data Here!B3", newCustomerValues.nameTwo);
-  await updateValues("Enter Data Here!B4", newCustomerValues.date);
-  await updateValues("Enter Data Here!B5", newCustomerValues.threePart);
-  await updateValues("Enter Data Here!B6", newCustomerValues.poNum);
-  await updateValues("Enter Data Here!B7", newCustomerValues.receivedBy);
-  await updateValues("Enter Data Here!B8", newCustomerValues.bolNum);
-  await updateValues("Enter Data Here!B9", newCustomerValues.carrier);
-  await updateValues("Enter Data Here!B10", newCustomerValues.lbs);
-  await updateValues("Enter Data Here!B11", newCustomerValues.totalCount);
-  await updateValues("Enter Data Here!B12", newCustomerValues.vendor);
-  
+  // Batch update
+  await googleSheets.spreadsheets.batchUpdate({
+    spreadsheetId,
+    resource: {
+      requests,
+    },
+  });
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: "Success" })
+    body: JSON.stringify({ message: "Success" }),
   };
 }
