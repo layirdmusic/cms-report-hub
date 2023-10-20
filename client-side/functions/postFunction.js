33b3065089de60838b4732f52b89c56b43089029
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-20 14:14:59 -0400

    update

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -59,7 +59,7 @@ export async function handler(event, context) {
       return googleSheets.spreadsheets.values.append({
         auth,
         spreadsheetId,
-        range: "A",
+        range: "Customer Database!B2:A",
         valueInputOption: "USER_ENTERED",
         values: [[values]],
       });
