49ac0de5771ac5f752f85334f3b234e257701f3a
Lejon Lewis <lejonlewis@Lejons-MacBook-Pro.local>
2023-10-20 13:11:41 -0400

    update

--- client-side/functions/postFunction.js
+++ client-side/functions/postFunction.js
@@ -111,8 +111,8 @@ export async function handler(event, context) {
   const append = []
 
   const customerAppend = [
-    ["Customer Database!A:A", "New Value 1"],
-    ["Customer Database!B:B", "New Value 2"],
+    ["Customer Database!A4", "New Value 1"],
+    ["Customer Database!B4", "New Value 2"],
 ];
 
 appendValues(customerAppend)
